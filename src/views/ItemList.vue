ee49363f950e28585bba021c85bde4ebcf6d2e7b
ymym98 <jimboyumi@gmail.com>
2023-04-20 11:55:31 +0900

    商品一覧を表示

---
+++ src/views/ItemList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    class="container mt-16 flex justify-between item-center mx-auto px-8 md:px-14 lg:px-14 w-full"
+  >
+    <div class="w-full">
+      <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-6">
+        <div v-for="hotel of hotelList" :key="hotel.index">
+          <img
+            class="w-full h-72 lg:h-64 object-cover rounded-md"
+            :src="hotel.hotelImageUrl"
+          />
+          <div class="text-left">
+            <div class="mt-3 text-sm font-bold">{{ hotel.hotelName }}</div>
+            <p class="text-xs text-gray-600">
+              {{ hotel.address1 + hotel.address2 }}
+            </p>
+            <p class="text-xs font-bold text-gray-600">
+              ¥ {{ hotel.hotelMinCharge }}~ /泊
+            </p>
+          </div>
+        </div>
+      </div>
+      <button @click="search()">表示</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hotelList: [],
+    };
+  },
+  methods: {
+    async search() {
+      // クエリーストリングを作成
+      const baseUrl =
+        "https://app.rakuten.co.jp/services/api/Travel/SimpleHotelSearch/20170426?";
+
+      const params = {
+        applicationId: "1043924183484318213",
+        largeClassCode: "japan",
+        middleClassCode: "tokyo",
+        smallClassCode: "tokyo", // 東京23区
+        detailClassCode: "A", //  東京駅・銀座・日本橋
+      };
+      const queryParams = new URLSearchParams(params);
+      console.log(baseUrl + queryParams);
+
+      // fetchでJSON取得
+      const response = await fetch(baseUrl + queryParams).then((response) =>
+        response.json()
+      );
+      console.log(response);
+      for (let i = 0; i < response.hotels.length; i++) {
+        const hotel = response.hotels[i].hotel[0].hotelBasicInfo;
+
+        this.hotelList.push(hotel);
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
