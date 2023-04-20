52fb46f8fd36597e86f28d5446c0aab9e231c46f
ymym98 <jimboyumi@gmail.com>
2023-04-20 14:42:37 +0900

    ヘッダーを追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,38 @@
 <template>
-  <div></div>
+  <div>
+    <header class="py-6">
+      <!-- ロゴ -->
+      <div
+        class="container mx-auto flex justify-between items-center px-8 md:px-14 lg:px-24 w-full"
+      >
+        <div class="text-lg font-bold">Travel.com</div>
+
+        <!-- 会員登録等 -->
+        <div class="space-x-12 hidden md:flex items-center">
+          <a
+            href="#portfolio"
+            class="hover:text-selected-text transition-all duration-300"
+            >ログイン</a
+          >
+          <a
+            href="#skills"
+            class="hover:text-selected-text transition-all duration-300"
+            >会員登録</a
+          >
+          <a href="#contact">
+            <button
+              class="px-6 py-2 bg-theme font-bold rounded-lg hover:bg-purple-600 transition-all duration-300"
+            >
+              お問い合わせ
+            </button>
+          </a>
+        </div>
+        <div class="md:hidden">
+          <i class="fa-solid fa-bars"></i>
+        </div>
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -1,26 +1,36 @@
 <template>
-  <div
-    class="container mt-16 flex justify-between item-center mx-auto px-8 md:px-14 lg:px-14 w-full"
-  >
-    <div class="w-full">
-      <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-6">
-        <div v-for="hotel of hotelList" :key="hotel.index">
-          <img
-            class="w-full h-72 lg:h-64 object-cover rounded-md"
-            :src="hotel.hotelImageUrl"
-          />
-          <div class="text-left">
-            <div class="mt-3 text-sm font-bold">{{ hotel.hotelName }}</div>
-            <p class="text-xs text-gray-600">
-              {{ hotel.address1 + hotel.address2 }}
-            </p>
-            <p class="text-xs font-bold text-gray-600">
-              ¥ {{ hotel.hotelMinCharge }}~ /泊
-            </p>
+  <div>
+    <!-- 検索フォーム -->
+    <div class="flex justify-center">
+      <input type="text" class="border-2 rounded-full h-9 w-64" />
+      <button type="submit" @click="search()">
+        <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+      </button>
+    </div>
+
+    <!-- ホテル一覧 -->
+    <div
+      class="container mt-16 flex justify-between item-center mx-auto px-8 md:px-14 lg:px-14 w-full"
+    >
+      <div class="w-full">
+        <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-6">
+          <div v-for="hotel of hotelList" :key="hotel.index">
+            <img
+              class="w-full h-72 lg:h-64 object-cover rounded-md"
+              :src="hotel.hotelImageUrl"
+            />
+            <div class="text-left">
+              <div class="mt-3 text-sm font-bold">{{ hotel.hotelName }}</div>
+              <p class="text-xs text-gray-600">
+                {{ hotel.address1 + hotel.address2 }}
+              </p>
+              <p class="text-xs font-bold text-gray-600">
+                ¥ {{ hotel.hotelMinCharge }}~ /泊
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <button @click="search()">表示</button>
     </div>
   </div>
 </template>
